06c5f4a04f0702c3e51af3a5c3a2dca8d360a930
Mark <m.a.cullen@outlook.com>
2023-12-30 10:03:24 +0000

    Add project files.

---
+++ CarbAndEggTimer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarbAndEggTimer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
